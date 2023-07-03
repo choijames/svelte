7fae8b8f5283f372c4e162650428f18a123f7f53
Jin <jin@capitect.com>
2023-07-03 10:27:28 +0900

    remove custom font sizes, add custom colors; add global spacing; remove styles from footer; run prettier, lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,19 +6,34 @@
 </script>
 
 <template lang="pug">
-
 	//- head
 	svelte:head
 		meta(charset="utf-8")
-		meta(name="description", content="A node starter template for the Svelte, Typescript, TailwindCSS, and Pug stack.")
-		meta(name="viewport", content="width=device-width")
+		meta(
+			content="A node starter template for the Svelte, Typescript, TailwindCSS, and Pug stack.",
+			name="description"
+		)
+		meta(
+			content="width=device-width",
+			name="viewport"
+		)
 
-		link(rel="preconnect", href="https://fonts.googleapis.com")
-		link(rel="preconnect", href="https://fonts.gstatic.com", crossorigin)
-		link(rel="stylesheet", href="Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap")
+		link(
+			href="https://fonts.googleapis.com",
+			rel="preconnect"
+		)
+		link(
+			crossorigin,
+			href="https://fonts.gstatic.com",
+			rel="preconnect"
+		)
+		link(
+			href="Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+			rel="stylesheet"
+		)
 
 		title hi
-		
+
 	//- body
 	main
 		p hi
